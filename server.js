37be0c16665ebceeb1cd18b89e61c5d18ba579b2
Donald Linnell <dlinnell@Donalds-MacBook-Air.local>
2018-10-01 09:38:43 -0500

    Updating password

--- server.js
+++ server.js
@@ -72,7 +72,7 @@ app.post('/api/submit', (req, res) => {
             service: 'gmail',
             auth: {
                 user: 'kippopsform@gmail.com',
-                pass: 'K!PpOp$form'
+                pass: 'K!TtY0954'
             }
         });
 
